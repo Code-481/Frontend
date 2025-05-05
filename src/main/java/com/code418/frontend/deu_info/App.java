ce7f43ab2662ed463111cc10d83c7576c16c3250
INMD1 <87979171+INMD1@users.noreply.github.com>
2025-05-05 23:11:05 +0900

    Instapp Package로 만들기 위한 작업

--- src/main/java/com/code418/frontend/deu_info/App.java
+++ src/main/java/com/code418/frontend/deu_info/App.java
@@ -5,7 +5,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.scene.web.WebView;
 import javafx.scene.web.WebEngine;
-import java.net.URL;
+import java.io.File;
 
 public class App extends Application {
 
@@ -24,17 +24,13 @@ public class App extends Application {
         } catch (NoSuchMethodError | Exception e) {
             alret.error("예외사항이 발생했습니다.", "현재 자바가 17미만 입니다! 자바 17로 설치 해주시기바람니다.");
         }
-        
-        // 리소스 경로 가져오기 (플랫폼 무관)
-        URL url = getClass().getResource("/static/Frontend/");
-        if (url != null) {
-            String path = url.getPath();
+
+        // 외부 리소스 폴더 경로 지정
+        String resourcePath = "Frontend"; // 실행 폴더 기준 상대경로
+        File resourceDir = new File(resourcePath);
+        if (resourceDir.exists() && resourceDir.isDirectory()) {
             int port = 8964;
-            if (path.startsWith("/") && path.length() > 2 && path.charAt(2) == ':') {
-                path = path.substring(1);
-            }
-            System.out.println(path);
-            httpservers server = new httpservers(path, port);
+            httpservers server = new httpservers(resourceDir.getAbsolutePath(), port);
             server.start();
 
             webView.getEngine().load("http://localhost:" + port);
@@ -44,16 +40,13 @@ public class App extends Application {
             stage.setTitle("JavaFX WebView 크로스 플랫폼 예제");
             stage.show();
 
-            stage.setOnCloseRequest(event -> {
-                server.stop();
-            });
+            stage.setOnCloseRequest(event -> server.stop());
         } else {
-            alret.error("예외사항이 발생했습니다.", "index 파일이 없습니다.");
+            alret.error("예외사항이 발생했습니다.", "frontend-dist 폴더가 없습니다.");
         }
     }
 
     public static void main(String[] args) {
-        //OS따라 폴더 구조가 다를수 있기 때문에 OS정보를 가지고옴
         if (System.getProperty("os.name").toLowerCase().contains("linux")) {
             System.setProperty("javafx.platform", "gtk");
         }
